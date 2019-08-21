1cebf9db77280c53e10f67943c473b6fd550f4f1
jamjamnewbie <jamjamerlin@gmail.com>
2019-08-21 15:48:39 +0800

    PS 1 b2

--- Problem Sets/1.c
+++ Problem Sets/1.c
@@ -10,24 +10,26 @@ int main() {
     srand(time(NULL));
     rand();
 
-    // Time Comparisons
-    float *averageTimeBubble = (float*) malloc(sizeInputsArrSize * sizeof(float));
-    float *averageTimeInsertion = (float*) malloc(sizeInputsArrSize * sizeof(float));
-    float *averageTimeSelection = (float*) malloc(sizeInputsArrSize * sizeof(float));
+    printf("Starting Time Tests...");
+
+    // Time Tests
+    double *averageTimeBubble = (double*) malloc(sizeInputsArrSize * sizeof(double));
+    double *averageTimeInsertion = (double*) malloc(sizeInputsArrSize * sizeof(double));
+    double *averageTimeSelection = (double*) malloc(sizeInputsArrSize * sizeof(double));
 
     for(int i = 0; i < sizeInputsArrSize; i++) {
         int N = sizeInputs[i];
         int *arr = malloc(N * sizeof(int));
         
         //Bubble Sort
-        float timeBubbleSort = 0.0;
+        double timeBubbleSort = 0.0;
         for(int i = 0; i < 30; i++) {
             //Randomizer
             for(int i = 0; i < N; i++) {
                 arr[i] = rand();
             }
 
-            int start = time(NULL);
+            clock_t start = clock();
             for(int i = N - 1; i > 0; i--) {
                 for(int j = 0; j < i; j++) {
                     if(arr[j] > arr[j + 1]) {
@@ -37,21 +39,21 @@ int main() {
                     }
                 }
             }
-            int end = time(NULL);
-            int duration = end - start;
+            clock_t end = clock();
+            double duration = (double) (end - start) / CLOCKS_PER_SEC;
             timeBubbleSort += duration;
         }
         averageTimeBubble[i] = timeBubbleSort / 30.0;
 
         //Insertion Sort
-        float timeInsertionSort = 0.0;
+        double timeInsertionSort = 0.0;
         for(int i = 0; i < 30; i++) {
             //Randomizer
             for(int i = 0; i < N; i++) {
                 arr[i] = rand();
             }
 
-            int start = time(NULL);
+            clock_t start = clock();
             for(int i = 1; i < N; i++) {
                 int tmp = arr[i], j = i - 1;
                 while(j >= 0 && arr[j] > tmp) {
@@ -60,21 +62,21 @@ int main() {
                 }
                 arr[j + 1] = tmp;
             }
-            int end = time(NULL);
-            int duration = end - start;
+            clock_t end = clock();
+            double duration = (double) (end - start) / CLOCKS_PER_SEC;
             timeInsertionSort += duration;
         }
         averageTimeInsertion[i] = timeInsertionSort / 30.0;
 
         //Selection Sort
-        float timeSelectionSort = 0.0;
+        double timeSelectionSort = 0.0;
         for(int i = 0; i < 30; i++) {
             //Randomizer
             for(int i = 0; i < N; i++) {
                 arr[i] = rand();
             }
 
-            int start = time(NULL);
+            clock_t start = clock();
             for(int i = 0; i < N; i++) {
                 int smallestIndex = i;
                 for(int j = i + 1; j < N; j++) {
@@ -85,16 +87,202 @@ int main() {
                 arr[i] = arr[smallestIndex];
                 arr[smallestIndex] = tmp;
             }
-            int end = time(NULL);
-            int duration = end - start;
+            clock_t end = clock();
+            double duration = (double) (end - start) / CLOCKS_PER_SEC;
             timeSelectionSort += duration;
         }
         averageTimeSelection[i] = timeSelectionSort / 30.0;
+        free(arr);
     }
 
+    printf("Completed\n");
+    printf("Starting Comparison Tests...");
+
+    // Comparison Tests
+    double *averageComparisonsBubble = (double*) malloc(sizeInputsArrSize * sizeof(double));
+    double *averageComparisonsInsertion = (double*) malloc(sizeInputsArrSize * sizeof(double));
+    double *averageComparisonsSelection = (double*) malloc(sizeInputsArrSize * sizeof(double));
+
     for(int i = 0; i < sizeInputsArrSize; i++) {
-        printf("Time Taken (%d) (Bubble Sort): %f s\n", sizeInputs[i], averageTimeBubble[i]);
-        printf("Time Taken (%d) (Insertion Sort): %f s\n", sizeInputs[i], averageTimeInsertion[i]);
-        printf("Time Taken (%d) (Selection Sort): %f s\n", sizeInputs[i], averageTimeSelection[i]);
+        int N = sizeInputs[i];
+        int *arr = malloc(N * sizeof(int));
+        
+        //Bubble Sort
+        int comparisonsBubbleSort = 0;
+        for(int i = 0; i < 30; i++) {
+            //Randomizer
+            for(int i = 0; i < N; i++) {
+                arr[i] = rand();
+            }
+
+            int comparisons = 0;
+            for(int i = N - 1; i > 0; i--) {
+                for(int j = 0; j < i; j++) {
+                    comparisons++;
+                    if(arr[j] > arr[j + 1]) {
+                        int tmp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = tmp;
+                    }
+                }
+            }
+            comparisonsBubbleSort += comparisons;
+        }
+        averageComparisonsBubble[i] = (double) comparisonsBubbleSort / 30.0;
+
+        //Insertion Sort
+        int comparisonsInsertionSort = 0;
+        for(int i = 0; i < 30; i++) {
+            //Randomizer
+            for(int i = 0; i < N; i++) {
+                arr[i] = rand();
+            }
+
+            int comparisons = 0;
+            for(int i = 1; i < N; i++) {
+                int tmp = arr[i], j = i - 1;
+                comparisons++;
+                while(j >= 0) {
+                    comparisons++;
+                    if(arr[j] > tmp) {
+                        arr[j + 1] = arr[j];
+                    } else {
+                        break;
+                    }
+                    comparisons++;
+                    j--;
+                }
+                arr[j + 1] = tmp;
+            }
+            comparisonsInsertionSort += comparisons;
+        }
+        averageComparisonsInsertion[i] = (double) comparisonsInsertionSort / 30.0;
+
+        //Selection Sort
+        int comparisonsSelectionSort = 0;
+        for(int i = 0; i < 30; i++) {
+            //Randomizer
+            for(int i = 0; i < N; i++) {
+                arr[i] = rand();
+            }
+
+            int comparisons = 0;
+            for(int i = 0; i < N; i++) {
+                int smallestIndex = i;
+                for(int j = i + 1; j < N; j++) {
+                    comparisons++;
+                    if(arr[j] < arr[smallestIndex]) {
+                        smallestIndex = j;
+                    }
+                }
+
+                int tmp = arr[i];
+                arr[i] = arr[smallestIndex];
+                arr[smallestIndex] = tmp;
+            }
+            comparisonsSelectionSort += comparisons;
+        }
+        averageComparisonsSelection[i] = (double) comparisonsSelectionSort / 30.0;
+        free(arr);
+    }
+
+    printf("Completed\n");
+    printf("Starting Swap Tests...");
+
+    // Swap Tests
+    double *averageSwapsBubble = (double*) malloc(sizeInputsArrSize * sizeof(double));
+    double *averageSwapsInsertion = (double*) malloc(sizeInputsArrSize * sizeof(double));
+    double *averageSwapsSelection = (double*) malloc(sizeInputsArrSize * sizeof(double));
+
+    for(int i = 0; i < sizeInputsArrSize; i++) {
+        int N = sizeInputs[i];
+        int *arr = malloc(N * sizeof(int));
+        
+        //Bubble Sort
+        int swapsBubbleSort = 0;
+        for(int i = 0; i < 30; i++) {
+            //Randomizer
+            for(int i = 0; i < N; i++) {
+                arr[i] = rand();
+            }
+
+            int swaps = 0;
+            for(int i = N - 1; i > 0; i--) {
+                for(int j = 0; j < i; j++) {
+                    if(arr[j] > arr[j + 1]) {
+                        swaps += 2;
+                        int tmp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = tmp;
+                    }
+                }
+            }
+            swapsBubbleSort += swaps;
+        }
+        averageSwapsBubble[i] = (double) swapsBubbleSort / 30.0;
+
+        //Insertion Sort
+        int swapsInsertionSort = 0;
+        for(int i = 0; i < 30; i++) {
+            //Randomizer
+            for(int i = 0; i < N; i++) {
+                arr[i] = rand();
+            }
+
+            int swaps = 0;
+            for(int i = 1; i < N; i++) {
+                int tmp = arr[i], j = i - 1;
+                while(j >= 0 && arr[j] > tmp) {
+                    swaps++;
+                    arr[j + 1] = arr[j];
+                    j--;
+                }
+                swaps++;
+                arr[j + 1] = tmp;
+            }
+            swapsInsertionSort += swaps;
+        }
+        averageSwapsInsertion[i] = (double) swapsInsertionSort / 30.0;
+
+        //Selection Sort
+        int swapsSelectionSort = 0;
+        for(int i = 0; i < 30; i++) {
+            //Randomizer
+            for(int i = 0; i < N; i++) {
+                arr[i] = rand();
+            }
+
+            int swaps = 0;
+            for(int i = 0; i < N; i++) {
+                int smallestIndex = i;
+                for(int j = i + 1; j < N; j++) {
+                    if(arr[j] < arr[smallestIndex]) {
+                        smallestIndex = j;
+                    }
+                }
+
+                if(smallestIndex != i) swaps += 2;
+                int tmp = arr[i];
+                arr[i] = arr[smallestIndex];
+                arr[smallestIndex] = tmp;
+            }
+            swapsSelectionSort += swaps;
+        }
+        averageSwapsSelection[i] = (double) swapsSelectionSort / 30.0;
+        free(arr);
+    }
+    printf("Completed\n");
+
+    //Output
+    for(int i = 0; i < sizeInputsArrSize; i++) {
+        printf("Time Taken (N = %d) (Bubble Sort): %f s\n", sizeInputs[i], averageTimeBubble[i]);
+        printf("Time Taken (N = %d) (Insertion Sort): %f s\n", sizeInputs[i], averageTimeInsertion[i]);
+        printf("Time Taken (N = %d) (Selection Sort): %f s\n", sizeInputs[i], averageTimeSelection[i]);
+        printf("Comparisons Taken (N = %d) (Bubble Sort): %.2f\n", sizeInputs[i], averageComparisonsBubble[i]);
+        printf("Comparisons Taken (N = %d) (Insertion Sort): %.2f\n", sizeInputs[i], averageComparisonsInsertion[i]);
+        printf("Comparisons Taken (N = %d) (Selection Sort): %.2f\n", sizeInputs[i], averageComparisonsSelection[i]);
+        printf("Swaps Taken (N = %d) (Bubble Sort): %.2f\n", sizeInputs[i], averageSwapsBubble[i]);
+        printf("Swaps Taken (N = %d) (Insertion Sort): %.2f\n", sizeInputs[i], averageSwapsInsertion[i]);
+        printf("Swaps Taken (N = %d) (Selection Sort): %.2f\n", sizeInputs[i], averageSwapsSelection[i]);
     }
 }
